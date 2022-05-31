03d24be428b5fbdc575e62708948e9ae6592173c
Anna Neufeld <annaneufeld@Annas-MacBook-Pro-3.local>
2022-05-31 10:35:22 -0700

    hi

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Cardiomyocite data. 
+#'
+#' This dataset was collected by Elborany et al., 2021 and used in our paper. 
+#'
+#' @docType data
+#'}
+#' @keywords datasets
+#' @format A RDS object. 
+#' @references 
+#' @examples
+#' data(cm)
+"cm"
